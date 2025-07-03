322fc623b7b34783ae08685f8d8480a25bd7b449
creativeloc <creativeloc@yahoo.com>
2025-07-03 01:48:44 -0400

    start stories with button component

---
+++ src/components/ButtonCounter.jsx
@@ -0,0 +1,11 @@
+import "./buttonCounter.css"
+
+export const ButtonCounter = ({ label, onClick, size }) => {
+  return (
+    <div>
+      <button className={`counter-button--${size}`} onClick={onClick}>
+        {label}
+      </button>
+    </div>
+  )
+}
---
+++ src/components/ButtonCounter.stories.jsx
@@ -0,0 +1,21 @@
+import { ButtonCounter } from "./ButtonCounter"
+
+export default {
+  title: "Counter/ButtonCounter",
+  component: ButtonCounter
+}
+
+// a story where the button size is large
+export const Large = {
+  args: {
+    size: "large",
+    label: "Click Me"
+  }
+}
+
+export const Small = {
+  args: {
+    size: "small",
+    label: "Click Me"
+  }
+}
