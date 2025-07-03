b58198892cf5a54d471e7889de37d114670cc924
Lawrence Caulker <locaulker@gmail.com>
2025-07-03 18:04:33 -0400

    add autodocs

--- src/components/ButtonCounter.stories.jsx
+++ src/components/ButtonCounter.stories.jsx
@@ -3,6 +3,7 @@ import { ButtonCounter } from "./ButtonCounter"
 export default {
   title: "Counter/ButtonCounter",
   component: ButtonCounter,
+  tags: ["autodocs"],
   argTypes: {
     backgroundColor: { control: "color" }
   }
