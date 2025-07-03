480b710d1e4ae74751784eaf498502813bacac00
Lawrence Caulker <locaulker@gmail.com>
2025-07-03 19:01:07 -0400

    add customization to buttons

--- src/components/ButtonCounter.stories.jsx
+++ src/components/ButtonCounter.stories.jsx
@@ -5,7 +5,19 @@ export default {
   component: ButtonCounter,
   tags: ["autodocs"],
   argTypes: {
-    backgroundColor: { control: "color" }
+    backgroundColor: {
+      control: "color",
+      description:
+        "Please change the background color within the blue spectrum",
+      table: {
+        defaultValue: { summary: "#c4fff3" }
+      }
+    },
+    label: {
+      control: "text",
+      table: { defaultValue: { summary: "Click Me" } },
+      description: "Other options include: Press Enter, Enter, Go!"
+    }
   }
 }
 
@@ -13,13 +25,29 @@ export default {
 export const Large = {
   args: {
     size: "large",
-    label: "Click Me"
+    label: "Click Me",
+    backgroundColor: "#c4fff3"
+  },
+  parameters: {
+    docs: {
+      description: {
+        story: "This is the large button view"
+      }
+    }
   }
 }
 
 export const Small = {
   args: {
     size: "small",
-    label: "Click Me"
+    label: "Click Me",
+    backgroundColor: "#c4fff3"
+  },
+  parameters: {
+    docs: {
+      description: {
+        story: "This is the small button view"
+      }
+    }
   }
 }
